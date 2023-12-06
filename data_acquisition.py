738662e82a8e9b059dd4cd4e1c2022e0ef6c5ab6
Vansh <99641079+v-code01@users.noreply.github.com>
2023-12-06 11:02:01 -0600

    Create data_acquisition.py

---
+++ data_acquisition.py
@@ -0,0 +1,17 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+def acquire_data():
+    api_url = "https://api.baseballstats.com"
+    response = requests.get(api_url)
+    api_data = response.json()
+
+    web_url = "https://baseballstats.com"
+    html_content = requests.get(web_url).content
+    soup = BeautifulSoup(html_content, 'html.parser')
+
+    web_data = pd.read_html(html_content)
+    processed_data = pd.concat(web_data)
+
+    return processed_data
